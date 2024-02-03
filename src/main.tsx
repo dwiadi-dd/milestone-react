f89448e2fbf0e631aad672f896169d44256242a2
adi bangkit <adibangkai@gmail.com>
2024-02-03 19:48:18 +0700

    add browsrouter path

--- src/main.tsx
+++ src/main.tsx
@@ -2,22 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Welcome from "./components/Welcome.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/Success",
-    element: <Welcome />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>{" "}
   </React.StrictMode>
 );
