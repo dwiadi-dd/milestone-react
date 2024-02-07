5466350363c9583e7fd9c6b029e6c483bb57b656
adi bangkit <adibangkai@gmail.com>
2024-02-07 09:11:52 +0700

    refractor userpage by each attribute

--- src/__test__/register.test.tsx
+++ src/__test__/register.test.tsx
@@ -1,12 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import { I18nextProvider } from "react-i18next";
 import UserDataContext from "../context/UserDataContext";
-import User from "../pages/user/index";
+import Register from "../pages/register/index";
 import i18next from "../locales/locales";
 import { expect, test } from "vitest";
 import { BrowserRouter } from "react-router-dom";
 
 const userData = {
+  username: "",
+  fullname: "",
+  email: "",
+  dob: "",
+  address: "",
+  city: "",
+  province: "",
+  zipcode: "",
+  password: "",
+};
+
+const userDataMock = {
   username: "testuser",
   fullname: "Test User",
   email: "testuser@example.com",
@@ -22,7 +34,7 @@ render(
   <BrowserRouter>
     <I18nextProvider i18n={i18next}>
       <UserDataContext.Provider value={[userData, () => {}]}>
-        <User />
+        <Register />
       </UserDataContext.Provider>
     </I18nextProvider>
   </BrowserRouter>
@@ -31,26 +43,4 @@ render(
 test("renders welcome message when user data is present", () => {
   const greeting = screen.getByTestId(`greeting-user`);
   expect(greeting.textContent).toEqual(`Welcome ${userData.username}!`);
-});
-
-test("renders email when user data is present", () => {
-  const email = screen.getByTestId(`test-email`);
-  expect(email.textContent).toEqual(`${userData.email}`);
-});
-
-test("renders dob when user data is present", () => {
-  const dob = screen.getByTestId(`test-dob`);
-  expect(dob.textContent).toEqual(`${userData.dob}`);
-});
-
-test("renders address when user data is present", () => {
-  const address = screen.getByTestId(`test-address`);
-  expect(address.textContent).toEqual(
-    `${userData.address}, ${userData.city}, ${userData.province}, ${userData.zipcode}. ${userData.password}`
-  );
-});
-
-test("renders fullname when user data is present", () => {
-  const fullname = screen.getByTestId(`test-fullname`);
-  expect(fullname.textContent).toEqual(`${userData.fullname}`);
 });
